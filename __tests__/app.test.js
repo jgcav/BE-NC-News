edea83ca8c4e234cba726b8dbf31fab110cc0a9e
Julian Cavanagh <juliancavanagh@hotmail.com>
2022-02-16 11:00:09 +0000

    add tests for PATCH /api/articles/:article_id #7

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -40,40 +40,92 @@ describe('GET /api/topics', () => {
     })
 });
 
-describe('GET /api/articles/:article_id', () => {
-    test('status 200: returns an article object with specified properties', () => {
-        return request(app)
-        .get('/api/articles/1')
-        .expect(200)
-        .then(({body: {article}}) => {
-            expect(article).toBeInstanceOf(Object)
-            expect(article).toEqual(
-                expect.objectContaining({
-                    author: expect.any(String),
-                    title: expect.any(String),
-                    article_id: expect.any(Number),
-                    body: expect.any(String),
-                    topic: expect.any(String),
-                    created_at: expect.any(String),
-                    votes: expect.any(Number)
+describe('/api/articles/:article_id', () => {
+    describe('GET', () => {
+        test('status 200: returns an article object with specified properties', () => {
+            return request(app)
+            .get('/api/articles/1')
+            .expect(200)
+            .then(({body: {article}}) => {
+                expect(article).toBeInstanceOf(Object)
+                expect(article).toEqual(
+                    expect.objectContaining({
+                        author: expect.any(String),
+                        title: expect.any(String),
+                        article_id: expect.any(Number),
+                        body: expect.any(String),
+                        topic: expect.any(String),
+                        created_at: expect.any(String),
+                        votes: expect.any(Number)
+                    })
+                )
+            })
+        });
+        test('status 400: returns a bad request error if the requested id is invalid', () => {
+            return request(app)
+            .get('/api/articles/invalid')
+            .expect(400)
+            .then(({body}) => {
+                expect(body.msg).toBe("Bad request. Invalid input.")
+            })
+        });
+        test('status 404: returns a not found error if the requested id is valid but does not exist', () => {
+            return request(app)
+            .get('/api/articles/13')
+            .expect(404)
+            .then(({body}) => {
+                expect(body.msg).toBe("ID 13 does not exist")
+            })
+        });
+    });
+    describe('PATCH', () => {
+        test('status 200: returns article with incremented votes property', () => {
+            return request(app)
+            .patch('/api/articles/1')
+            .send({inc_votes: 50})
+            .expect(200)
+            .then(({body}) => {
+                expect(body.response[0].votes).toBe(150)
+                expect(body.response[0]).toEqual(
+                    expect.objectContaining({
+                        author: expect.any(String),
+                        title: expect.any(String),
+                        article_id: expect.any(Number),
+                        body: expect.any(String),
+                        topic: expect.any(String),
+                        created_at: expect.any(String),
+                        votes: expect.any(Number)
+                    })
+                )
+            })
+            })
+            test('status 400: returns a bad request error if request is made with invalid request object', () => {
+                return request(app)
+                .patch('/api/articles/1')
+                .send({inc__votes: 50})
+                .expect(400)
+                .then(({body}) => {
+                    expect(body.msg).toBe("Bad request. Invalid input.")
                 })
-            )
-        })
+            });
+            test('status 400: returns a bad request error if request is made with invalid article_id', () => {
+                return request(app)
+                .patch('/api/articles/invalid')
+                .send({inc_votes: 50})
+                .expect(400)
+                .then(({body}) => {
+                    expect(body.msg).toBe("Bad request. Invalid input.")
+                })
+            });
+            test('status 404: returns a not found error if requested id is valid but does not exist', () => {
+                return request(app)
+                .patch('/api/articles/108')
+                .send({inc_votes: 50})
+                .expect(404)
+                .then(({body}) => {
+                expect(body.msg).toBe("ID 108 does not exist")
+                })
+            });
     });
-    test('status 400: returns a bad request error if the requested id is invalid', () => {
-        return request(app)
-        .get('/api/articles/invalid')
-        .expect(400)
-        .then(({body}) => {
-            expect(body.msg).toBe("Bad request. Invalid input.")
-        })
-    });
-    test('status 404: returns a not found error if the requested id is valid but does not exist', () => {
-        return request(app)
-        .get('/api/articles/13')
-        .expect(404)
-        .then(({body}) => {
-            expect(body.msg).toBe("ID 13 does not exist")
-        })
-    });
+    
 });
