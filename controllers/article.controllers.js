981bc6bfc2d18a41d992ece77359f3c5636f1ced
Julian Cavanagh <juliancavanagh@hotmail.com>
2022-02-16 12:28:06 +0000

    restructure - create article.controllers file

---
+++ controllers/article.controllers.js
@@ -0,0 +1,19 @@
+const {fetchArticleById, amendArticleVotesById} = require('../models/article.models')
+
+exports.getArticleById = (req, res, next) => {
+    const {article_id} = req.params
+    fetchArticleById(article_id)
+    .then((response) => {
+        res.status(200).send(response)
+    }).catch(next)
+}
+
+exports.updateArticleVotesById = (req, res, next) => {
+    const {article_id} = req.params
+    const updateVotes = req.body
+    const votes =  updateVotes.inc_votes
+    amendArticleVotesById(article_id, votes)
+    .then((response) => {
+        res.status(200).send({response})
+    }).catch(next)
+}
