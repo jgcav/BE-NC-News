243e132f6629935a94cabb3592ddb7f1b8ce3629
Julian Cavanagh <juliancavanagh@hotmail.com>
2022-02-18 09:02:54 +0000

    add app.get for /api/articles/:article_id/comments

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const {getTopics} = require('./controllers/topic.controllers')
 const {getArticleById, updateArticleVotesById, getArticles} = require('./controllers/article.controllers')
 const {getUsers} = require('./controllers/user.controllers')
+const {getCommentsByArticleId} = require('./controllers/comment.controllers')
 const {handle404, handleCustomErrors, handlePsqlErrors} = require('./errors/errors')
 
 const app = express()
@@ -13,6 +14,7 @@ app.get('/api/articles/:article_id', getArticleById)
 app.patch('/api/articles/:article_id', updateArticleVotesById)
 app.get('/api/users', getUsers)
 app.get('/api/articles', getArticles)
+app.get('/api/articles/:article_id/comments', getCommentsByArticleId)
 
 app.all('/*', handle404)
 
