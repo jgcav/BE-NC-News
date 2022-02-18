c2a10de52be7c4ff6f375d905ebdc5757ba19867
Julian Cavanagh <juliancavanagh@hotmail.com>
2022-02-18 15:10:54 +0000

    remove error handler from fetchCommentsByArticleId

--- models/comment.models.js
+++ models/comment.models.js
@@ -6,13 +6,6 @@ exports.fetchCommentsByArticleId = (article_id) => {
     FROM comments
     WHERE article_id = $1;`, [article_id])
     .then(({rows}) => {
-        
-        if (rows.length === 0){
-            return Promise.reject({
-                status: 404,
-                msg: `No comments found for article ID ${article_id}`
-            })
-        }
-        return rows
+           return rows
     })
 }
