b45b7940d694558ec277a0de39411ed91913cd21
Julian Cavanagh <juliancavanagh@hotmail.com>
2022-02-27 21:11:51 +0000

    create add/removeCommentsByArticleId models

--- models/comment.models.js
+++ models/comment.models.js
@@ -9,3 +9,31 @@ exports.fetchCommentsByArticleId = (article_id) => {
            return rows
     })
 }
+
+exports.addCommentsByArticleId = (article_id, userComment) => {
+    const {username, body} = userComment
+    if(!username || !body){
+        return Promise.reject({status: 400, msg: "Bad request. Invalid input."})
+    }
+    return db.query(`INSERT INTO comments
+    (body, author, article_id)
+    VALUES ($1, $2, $3)
+    RETURNING *;`,
+    [body, username, article_id])
+    .then(({rows}) => {
+        return rows[0];
+    })
+}
+
+exports.removeCommentsByCommentId = (comment_id) => {
+    return db.query(`DELETE FROM comments
+    WHERE comment_id = $1;`, [comment_id])
+    .then((result) => {
+        if(result.rowCount === 0){
+            return Promise.reject({
+                status: 404,
+                msg: "Comment ID not found"
+            })
+        }
+    })
+}
