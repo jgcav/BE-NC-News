3df0d58db0cfa83275bfe7fefcf4e60c06f2e251
Julian Cavanagh <juliancavanagh@hotmail.com>
2022-02-27 21:14:12 +0000

    add post/deleteComments ByArticle/CommentId

--- controllers/comment.controllers.js
+++ controllers/comment.controllers.js
@@ -1,10 +1,27 @@
 const { checkArticleExists } = require('../db/helpers/utils')
-const { fetchCommentsByArticleId } = require('../models/comment.models')
+const { fetchCommentsByArticleId, addCommentsByArticleId, removeCommentsByCommentId } = require('../models/comment.models')
 
 exports.getCommentsByArticleId = (req, res, next) => {
     const {article_id} = req.params
     Promise.all([fetchCommentsByArticleId(article_id), checkArticleExists(article_id)])
     .then(([comments]) => {
         res.status(200).send({comments})
+    }).catch(next)
+}
+
+exports.postCommentsByArticleId = (req, res, next) => {
+    const {article_id: article_id} = req.params
+    const userComment = req.body
+    addCommentsByArticleId(article_id, userComment)
+    .then((comment) => {
+        res.status(201).send({comment})
+    }).catch(next)
+}
+
+exports.deleteCommentsByCommentId = (req, res, next) => {
+    const {comment_id} = req.params
+    removeCommentsByCommentId(comment_id)
+    .then(() => {
+        res.status(204).send()
     }).catch(next)
 }
