4bc7bf134ca6dda0fda30032d13ef5dc9dcc710e
Julian Cavanagh <juliancavanagh@hotmail.com>
2022-02-27 21:12:47 +0000

    amend handlePsqlErrors function

--- errors/errors.js
+++ errors/errors.js
@@ -1,6 +1,8 @@
 exports.handlePsqlErrors = (err, req, res, next) => {
         if(err.code === "22P02") {
             res.status(400).send({msg: "Bad request. Invalid input."})
+        } else if(err.code === "23503") {
+            res.status(404).send({msg: "Requested input does not exist"})
         } else next(err)
     }
 
@@ -8,7 +10,7 @@ exports.handleCustomErrors = (err, req, res, next) => {
         if(err.status) {
             res.status(err.status).send({msg: err.msg})
         } else next(err)
-    }            
+    }    
 
 exports.handle404 = (req, res, next) => {
         res.status(404).send("Invalid path - page not found")
